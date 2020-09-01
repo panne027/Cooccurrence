d5612a08a810073b0aafec52b80b27b79816ef92
panne027 <70606912+panne027@users.noreply.github.com>
2020-09-01 15:55:10 -0500

    Add files via upload

---
+++ Non-Compliant-Window-Cooccurrence-Miner-master/Non-Compliant-Window-Cooccurrence-Miner-master/EngineReading.java
@@ -0,0 +1,29 @@
+
+public class EngineReading {
+
+	public long Iteration;
+	public int year;
+	public int month;
+	public int day;
+	public double Latitude;
+	public double Longitude;
+	public double SCRoutppm;
+	public double SCRoutgps;
+	public double Bkpwr;
+	public int pass;
+	
+	public String [] patternDimensionsValues; //the values corresponding to the dimensions of the pattern (after discretization into events) in the order they are stored in the dimensionsInfo[] array in the MainDriver class
+	
+	public void parseLine(String line)
+	{
+		String [] values = line.split(",");
+		Iteration = Long.parseLong(values[0]);
+		Latitude = Double.parseDouble(values[1]);
+		Longitude = Double.parseDouble(values[2]);
+		SCRoutgps = Double.parseDouble(values[3]);
+		Bkpwr = Double.parseDouble(values[4]);
+	
+		//TODO: parse pattern dimensions into array
+	}
+}
+
